85d3cbd0e5cb964bbd59cf46ef60a5e23a07e651
Sahil Tamboli <sahiltamboli303@gmail.com>
2026-02-14 18:49:24 +0530

    Refactor sum of squares functionality and add new project
    Updated `RunForLoopProgram` to compute the sum of squares by renaming `PrintSquareOfNaturalNos` to `PrintSumOfSqOfNaturalNos`. Adjusted the corresponding declaration in `ForLoopProgram.h`.
    
    Introduced a new project "StandardTemplateLibrary" with a basic "Hello World!" implementation in `StandardTemplateLibrary.cpp`, along with its project and filter files.

--- CPPBasics/ForLoopProgram.h
+++ CPPBasics/ForLoopProgram.h
@@ -4,7 +4,7 @@ void Print1tonNumbers(int n);
 void PrintSquareOfNnumbers(int n);
 void PrintEvenNumbersFrom2ToN(int n);
 void PrintOddNumbersFrom1ToN(int n);
-int PrintSquareOfNaturalNos(int n);
+int PrintSumOfSqOfNaturalNos(int n);
 unsigned long long Factorial(int n);
 void isPrime(int n);
 int FibonacciNumber(int n);
